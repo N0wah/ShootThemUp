d520563037e8e634a00db546f6786d465b2a5f2e
N0wah <noah68mollet@gmail.com>
2024-05-06 10:31:57 +0200

    Shoot System

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 import Player from './Player.js';
 
+
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 1280;
 canvas.height = 720;
 
-const player = new Player(canvas.width / 2, canvas.height / 2);
+const player = new Player(canvas.width / 2, canvas.height / 2, canvas);
 
 function gameLoop() {
     ctx.clearRect(0,0,canvas.width, canvas.height)
     player.draw(ctx)
+
 }
 
 
