2235909071991660f6df71d5ac41e6d120620d36
Along <144777531+AlongkornFR@users.noreply.github.com>
2024-05-06 11:30:45 +0200

    enemy

---
+++ Enemy.js
@@ -0,0 +1,36 @@
+export default class Enemy {
+    constructor({position,target,life}) {
+        this.position = position
+        this.width = 20;
+        this.height = 50;
+        this.speed = 2
+        this.target = target
+        this.life = life
+    }
+
+    draw(ctx){
+        
+        ctx.strokeStyle='red';
+        ctx.strokeRect(this.position.x, this.position.y, this.width, this.height);
+        ctx.fillStyle='red';
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+        this.move();
+    }
+
+    move(){
+            if(this.target.y > this.position.y){
+                this.position.y += this.speed
+            }
+            if (this.target.y < this.position.y){
+                this.position.y -= this.speed
+            }
+            if(this.target.x < this.position.x){
+                this.position.x -= this.speed
+            }
+            if(this.target.x > this.position.x){
+                this.position.x += this.speed
+            }
+           
+    }
+
+}
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import Player from './Player.js';
+import Enemy from './Enemy.js';
 
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
@@ -6,11 +7,71 @@ const ctx = canvas.getContext("2d");
 canvas.width = 1280;
 canvas.height = 720;
 
+function getRandom(){
+    var choose = Math.random()
+    console.log(choose);
+    if (choose <=0.5){
+        _x = randomPosX()[0]
+        _y = randomPosX()[1]
+    } else{
+        _x = randomPosY()[0]
+        _y = randomPosY()[1]
+    }
+}
+
+var _x;
+var _y;
+
+function randomPosY() {
+    var x = Math.random() * (1310-(-10))-10
+    var y = Math.random() * (740-(-10))-10
+    if (x > 0 || x < 1280){
+        while (y > 0 && y < 720){
+            var y = Math.random() * (740-(-10))-10
+        }
+    }
+    let x_y = [x, y]
+    return x_y
+}
+
+function randomPosX() {
+    var x = Math.random() * (1310-(-10))-10
+    var y = Math.random() * (740-(-10))-10
+    if (y > 0 || y < 720){
+        while (x > 0 && x < 1280){
+            var x = Math.random() * (1310-(-10))-10
+        }
+    }
+    let x_y = [x, y]
+    return x_y
+}
+
+getRandom();
+
 const player = new Player(canvas.width / 2, canvas.height / 2);
+const enemy = new Enemy({
+    position : {
+        x: _x,
+        y: _y
+    },
+    target : {
+        x: player.x,
+        y : player.y
+    },
+    life: {
+        health: 50,
+    }
+})
+
+
 
 function gameLoop() {
+    console.log(enemy.position)
+    enemy.target.x = player.x
+    enemy.target.y = player.y
     ctx.clearRect(0,0,canvas.width, canvas.height)
     player.draw(ctx)
+    enemy.draw(ctx)
 }
 
 
